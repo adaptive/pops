5b82ce48621373da564fb782141ccbf25596db0e
Hugo Romano <hugoromano@gmail.com>
2020-09-17 01:57:48 +0100

    iata airport import

--- tools/iata-filter.js
+++ tools/iata-filter.js
@@ -1,11 +1,11 @@
 import data from "../data/index.js";
-import iata from "../data/iata-unfiltered.js";
+import iata from "@adaptivelink/iata";
 
 const keys = Object.keys(data.providers);
 let pops = [];
 for (const key of keys) {
   const map1 = new Map(
-    [...iata].filter(([k]) => data.providers[key].pops.includes(k))
+    [...iata.airports].filter(([k]) => data.providers[key].pops.includes(k))
   );
 
   for (let value of map1.keys()) {
@@ -17,7 +17,7 @@ let unique_pops = [...new Set(pops)].sort();
 console.log(`/** IATA airports location filtered*/
 const iata = new Map([`);
 for (let e of unique_pops) {
-  console.log(`["${e}",[${iata.get(e)}]],`);
+  console.log(`["${e}",[${iata.airports.get(e)}]],`);
 }
 console.log(`]);
 
