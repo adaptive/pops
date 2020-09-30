751755f8a877099228f836f5fc477ad9da4c0c79
Hugo Romano <hugoromano@gmail.com>
2020-09-30 14:13:03 +0100

    spotters in tools

--- tools/spotters/cloudflare.js
+++ tools/spotters/cloudflare.js
@@ -1,11 +1,11 @@
 import _ from "lodash";
 import puppeteer from "puppeteer";
-import fastly from "../data/providers/fastly.js";
+import cloudflare from "../../data/providers/cloudflare.js";
 
 const spotter = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://status.fastly.com/");
+  await page.goto("https://www.cloudflarestatus.com/");
   const data = await page.$eval(
     "body > div.layout-content.status.status-index.starter > div.container > div.components-section.font-regular",
     e => e.innerHTML
@@ -18,25 +18,21 @@ spotter()
   .then(value => value.match(/\((.{3})\)/gm).map(x => x.slice(1, -1)))
   .then(a =>
     a.map(x => {
-      // hardcoding LON to LGW
-      if (x === "LON") return "LGW";
-      else return x;
-    })
-  )
-  .then(a =>
-    a.map(x => {
-      // hardcoding WDC to LEE
-      if (x === "WDC") return "LEE";
+      // hardcoding array due to Cloudflare Error
+      if (x === "NBG") return "NGB";
       else return x;
     })
   )
   .then(extracted => {
-    if (_.isEqual(extracted.sort(), fastly.pops)) {
-      console.log("fastly:success", extracted.length - fastly.pops.length);
+    if (_.isEqual(extracted.sort(), cloudflare.pops)) {
+      console.log(
+        "Cloudflare:success",
+        extracted.length - cloudflare.pops.length
+      );
     } else {
       console.log(
-        "fastly:error",
-        extracted.filter(e => !fastly.pops.includes(e))
+        "Cloudflare:error",
+        extracted.filter(e => !cloudflare.pops.includes(e))
       );
       throw new Error("Possible new PoP");
     }
--- tools/spotters/fastly.js
+++ tools/spotters/fastly.js
@@ -1,11 +1,11 @@
 import _ from "lodash";
 import puppeteer from "puppeteer";
-import cloudflare from "../data/providers/cloudflare.js";
+import fastly from "../../data/providers/fastly.js";
 
 const spotter = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://www.cloudflarestatus.com/");
+  await page.goto("https://status.fastly.com/");
   const data = await page.$eval(
     "body > div.layout-content.status.status-index.starter > div.container > div.components-section.font-regular",
     e => e.innerHTML
@@ -18,21 +18,25 @@ spotter()
   .then(value => value.match(/\((.{3})\)/gm).map(x => x.slice(1, -1)))
   .then(a =>
     a.map(x => {
-      // hardcoding array due to Cloudflare Error
-      if (x === "NBG") return "NGB";
+      // hardcoding LON to LGW
+      if (x === "LON") return "LGW";
+      else return x;
+    })
+  )
+  .then(a =>
+    a.map(x => {
+      // hardcoding WDC to LEE
+      if (x === "WDC") return "LEE";
       else return x;
     })
   )
   .then(extracted => {
-    if (_.isEqual(extracted.sort(), cloudflare.pops)) {
-      console.log(
-        "Cloudflare:success",
-        extracted.length - cloudflare.pops.length
-      );
+    if (_.isEqual(extracted.sort(), fastly.pops)) {
+      console.log("fastly:success", extracted.length - fastly.pops.length);
     } else {
       console.log(
-        "Cloudflare:error",
-        extracted.filter(e => !cloudflare.pops.includes(e))
+        "fastly:error",
+        extracted.filter(e => !fastly.pops.includes(e))
       );
       throw new Error("Possible new PoP");
     }
