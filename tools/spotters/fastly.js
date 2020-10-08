2cb7aecdbe648026b78c78fc9ebeccb3fe5ab7ec
Hugo Romano <hugoromano@gmail.com>
2020-10-08 04:55:25 +0100

    telegram tests

--- tools/spotters/fastly.js
+++ tools/spotters/fastly.js
@@ -1,6 +1,10 @@
 import _ from "lodash";
+import fetch from "isomorphic-fetch";
 import puppeteer from "puppeteer";
 import fastly from "../../data/providers/fastly.js";
+
+const user = process.env.TELEGRAM_USER
+const token = process.env.TELEGRAM_TOKEN;
 
 const spotter = async () => {
   const browser = await puppeteer.launch();
@@ -19,7 +23,7 @@ spotter()
   .then(a =>
     a.map(x => {
       // hardcoding LON to LGW
-      if (x === "LON") return "LGW";
+      if (x === "LON") return "LGWA";
       else return x;
     })
   )
@@ -34,10 +38,19 @@ spotter()
     if (_.isEqual(extracted.sort(), fastly.pops)) {
       console.log("fastly:success", extracted.length - fastly.pops.length);
     } else {
+      fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          chat_id: user,
+          text: `🕵️ fastly`
+        })
+      });
       console.log(
         "fastly:error",
         extracted.filter(e => !fastly.pops.includes(e))
       );
-      throw new Error("Possible new PoP");
     }
   });
