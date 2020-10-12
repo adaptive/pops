79217dcff428a65d24a65815c18acaf6071eda9c
Kevin Lu <18564130+KevinLu@users.noreply.github.com>
2020-10-12 12:25:05 -0400

    Add IATA codes for Google Compute Engine PoPs

---
+++ data/providers/googlecompute.js
@@ -0,0 +1,35 @@
+/**
+ *  https://cloud.google.com/cdn/docs/locations
+ */
+const googlecompute = {
+  name: "Google Compute Engine",
+  url: "https://cloud.google.com/compute",
+  pops: [
+    "AMS",
+    "BOM",
+    "BRU",
+    "CGK",
+    "CHA",
+    "DSM",
+    "FRA",
+    "GRU",
+    "HEL",
+    "HKG",
+    "IAD",
+    "ICN",
+    "KIX",
+    "LAS",
+    "LAX",
+    "LHR",
+    "NRT",
+    "PDX",
+    "SIN",
+    "SLC",
+    "SYD",
+    "TPE",
+    "YUL",
+    "ZRH"
+  ]
+};
+
+export default googlecompute;
