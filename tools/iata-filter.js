a9ffbb10e69b1d3d30934237912b1ca68f75dbb0
Hugo Romano <hugoromano@gmail.com>
2020-10-30 21:45:13 +0000

    reduce geo precision

--- tools/iata-filter.js
+++ tools/iata-filter.js
@@ -17,7 +17,10 @@ let unique_pops = [...new Set(pops)].sort();
 console.log(`/** IATA airports location filtered*/
 const iata = new Map([`);
 for (let e of unique_pops) {
-  console.log(`["${e}",[${iata.airports.get(e)}]],`);
+  let geo = iata.airports.get(e);
+  let lat = Math.round(geo[0] * 100) / 100;
+  let long = Math.round(geo[1] * 100) / 100;
+  console.log(`  ["${e}", [${lat}, ${long}]],`);
 }
 console.log(`]);
 
