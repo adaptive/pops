671c6d611c23400b08e5579f67340ba4281ab57e
Hugo Romano <hugoromano@gmail.com>
2021-03-23 15:51:44 +0000

    upcloud added SYD

--- data/providers/upcloud.js
+++ data/providers/upcloud.js
@@ -1,7 +1,19 @@
 const upcloud = {
   name: "UpCloud",
   url: "https://upcloud.com/",
-  pops: ["AMS", "FRA", "HEL", "JFK", "LHR", "MAD", "ORD", "SIN", "SJC", "WAW"]
+  pops: [
+    "AMS",
+    "FRA",
+    "HEL",
+    "JFK",
+    "LHR",
+    "MAD",
+    "ORD",
+    "SIN",
+    "SJC",
+    "SYD",
+    "WAW"
+  ]
 };
 
 export default upcloud;
