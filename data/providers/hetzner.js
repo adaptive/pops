aabedd3b5c640a884941e6b740d8eb155ca9694f
Hugo Romano <hugoromano@gmail.com>
2022-12-06 16:28:30 +0000

    hetzner HIO IAD

--- data/providers/hetzner.js
+++ data/providers/hetzner.js
@@ -3,6 +3,8 @@ const hetzner = {
   url: "https://www.hetzner.com/cloud",
   pops: [
     "HEL", // Helsinki (Finland)
+    "HIO", // Portland (USA)
+    "IAD", // Washington (USA)
     "NUE", // Nuremberg (Germany)
     "ZPM" // Potsdam (Germany)
   ]
