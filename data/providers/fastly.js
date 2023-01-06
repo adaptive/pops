10252ffc614ea0b3ebf7d8526511440e650d8df2
Hugo Romano <hugoromano@gmail.com>
2023-01-06 20:43:10 +0000

    fastly spotter

--- data/providers/fastly.js
+++ data/providers/fastly.js
@@ -2,102 +2,102 @@ const fastly = {
   name: "Fastly",
   url: "https://www.fastly.com",
   pops: [
-    "ACC",
-    "ADL",
-    "AKL",
-    "AMS",
-    "ATL",
-    "BFI",
-    "BKK",
-    "BMA",
-    "BNE",
-    "BOG",
-    "BOM",
-    "BOS",
-    "BRU",
-    "BUR",
-    "CCU",
-    "CDG",
-    "CGH",
-    "CHC",
-    "CHI",
-    "CMH",
-    "CPH",
-    "CPT",
-    "CWB",
-    "DAL",
-    "DEL",
-    "DEN",
-    "DFW",
-    "DTW",
-    "DUB",
-    "DXB",
-    "EWR",
-    "EZE",
-    "FCO",
-    "FJR",
-    "FOR",
-    "FRA",
-    "FTY",
-    "GIG",
-    "GNV",
-    "HEL",
-    "HHN",
-    "HKG",
-    "HND",
-    "HNL",
-    "HYD",
-    "IAD",
-    "IAH",
-    "ICN",
-    "ITM",
-    "JNB",
-    "KUL",
-    "LAX",
-    "LCK",
-    "LCY",
-    "LEE",
-    "LGA",
-    "LGB",
-    "LGW",
-    "LHR",
-    "LIM",
-    "LIN",
-    "LIS",
-    "MAA",
-    "MAD",
-    "MAN",
-    "MCI",
-    "MEL",
-    "MIA",
-    "MNL",
-    "MRS",
-    "MSP",
-    "MUC",
-    "MXP",
-    "NRT",
-    "OSL",
-    "PAO",
-    "PDK",
-    "PDX",
-    "PER",
-    "PHX",
-    "PMO",
-    "QPG",
-    "SCL",
-    "SEA",
-    "SJC",
-    "SOF",
-    "STL",
-    "STP",
-    "SYD",
-    "TYO",
-    "VIE",
-    "WLG",
-    "YUL",
-    "YVR",
-    "YYC",
-    "YYZ"
+    "ACC", // Accra (Ghana)
+    "ADL", // Adelaide (Australia)
+    "AKL", // Auckland (New Zealand)
+    "AMS", // Amsterdam (Netherlands)
+    "ATL", // Atlanta (US)
+    "BKK", // Bangkok (Thailand)
+    "BMA", // Stockholm (Sweden)
+    "BNE", // Brisbane (Australia)
+    "BOG", // Bogota (Colombia)
+    "BOM", // Mumbai (India)
+    "BOS", // Boston (US)
+    "BRU", // Brussels (Belgium)
+    "BUR", // Burbank (US)
+    "CCU", // Kolkata (India)
+    "CDG", // Paris (France)
+    "CGH", // Sao Paulo (Brazil)
+    "CHC", // Christchurch (New Zealand)
+    "CHI", // Chicago (US)
+    "CMH", // Columbus (US)
+    "CPH", // Copenhagen (Denmark)
+    "CPT", // Cape Town (South Africa)
+    "CWB", // Curitiba (Brazil)
+    "DAL", // Dallas (US)
+    "DEL", // Delhi (India)
+    "DEN", // Denver (US)
+    "DFW", // Dallas (US)
+    "DTW", // Detroit (US)
+    "DUB", // Dublin (Ireland)
+    "DXB", // Dubai (United Arab Emirates)
+    "EWR", // Newark (US)
+    "EZE", // Buenos Aires (Argentina)
+    "FCO", // Rome (Italy)
+    "FJR", // Fujairah (United Arab Emirates)
+    "FOR", // Fortaleza (Brazil)
+    "FRA", // Frankfurt (Germany)
+    "FTY", // Atlanta (US)
+    "GIG", // Rio de Janeiro (Brazil)
+    "GNV", // Gainesville (US)
+    "GRU", // Sao Paulo (Brazil)
+    "HEL", // Helsinki (Finland)
+    "HHN", // Frankfurt (Germany)
+    "HKG", // Hong Kong (China)
+    "HND", // Tokyo (Japan)
+    "HNL", // Honolulu (US)
+    "HYD", // Hyderabad (India)
+    "IAD", // Washington DC (US)
+    "IAH", // Houston (US)
+    "ICN", // Seoul (South Korea)
+    "ITM", // Osaka (Japan)
+    "JFK", // New York (US)
+    "JNB", // Johannesburg (South Africa)
+    "KUL", // Kuala Lumpur (Malaysia)
+    "LAX", // Los Angeles (US)
+    "LCK", // Columbus (US)
+    "LCY", // London (UK)
+    "LEE", // Leesburg (US)
+    "LGA", // New York (US)
+    "LGW", // London (UK)
+    "LHR", // London (UK)
+    "LIM", // Lima (Peru)
+    "LIN", // Milan (Italy)
+    "LIS", // Lisbon (Portugal)
+    "MAA", // Chennai (India)
+    "MAD", // Madrid (Spain)
+    "MAN", // Manchester (UK)
+    "MCI", // Kansas City (US)
+    "MEL", // Melbourne (Australia)
+    "MIA", // Miami (US)
+    "MNL", // Manila (Philippines)
+    "MRS", // Marseille (France)
+    "MSP", // Minneapolis (US)
+    "MUC", // Munich (Germany)
+    "MXP", // Milan (Italy)
+    "NRT", // Tokyo (Japan)
+    "OSL", // Oslo (Norway)
+    "PAO", // Palo Alto (US)
+    "PDK", // Atlanta (US)
+    "PDX", // Portland (US)
+    "PER", // Perth (Australia)
+    "PHX", // Phoenix (US)
+    "PMO", // Palermo (Italy)
+    "QPG", // Singapore (Singapore)
+    "SCL", // Santiago (Chile)
+    "SEA", // Seattle (US)
+    "SJC", // San Jose (US)
+    "SOF", // Sofia (Bulgaria)
+    "STL", // St. Louis (US)
+    "STP", // St. Paul (US)
+    "SYD", // Sydney (Australia)
+    "TYO", // Tokyo (Japan)
+    "VIE", // Vienna (Austria)
+    "WLG", // Wellington (New Zealand)
+    "YUL", // Montreal (Canada)
+    "YVR", // Vancouver (Canada)
+    "YYC", // Calgary (Canada)
+    "YYZ" // Toronto (Canada)
   ]
 };
 
--- tools/spotters/fastly.js
+++ tools/spotters/fastly.js
@@ -1,54 +1,73 @@
 import _ from "lodash";
-import puppeteer from "puppeteer";
+import fetch from "isomorphic-fetch";
 import toTelegram from "./lib/telegram.js";
 import provider from "../../data/providers/fastly.js";
 
 const asset = "fastly";
 
+const regex = /\((.{3,7})\)/gm;
+
 const spotter = async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("https://status.fastly.com/");
-  const data = await page.$eval(
-    "body > div.layout-content.status.status-index.starter > div.container > div.components-section.font-regular",
-    e => e.innerHTML
-  );
-  browser.close();
-  return data;
+  await fetch(`https://www.fastlystatus.com/components.js`, {
+    method: "GET"
+  })
+    .then(response => response.text())
+    .then(value => value.match(/\((.{3})\)/gm).map(x => x.slice(1, 4)))
+    .then(a => _.uniq(a))
+    .then(a =>
+      a.map(x => {
+        // hardcoding LON to LGW
+        if (x === "LON") return "LGW";
+        else return x;
+      })
+    )
+    .then(a =>
+      a.map(x => {
+        // hardcoding WDC to LEE
+        if (x === "WDC") return "LEE";
+        else return x;
+      })
+    )
+    .then(a =>
+      a.map(x => {
+        // hardcoding BFI to SEA
+        if (x === "BFI") return "SEA";
+        else return x;
+      })
+    )
+    .then(a =>
+      a.map(x => {
+        // hardcoding NYC to JFK
+        if (x === "NYC") return "JFK";
+        else return x;
+      })
+    )
+    .then(a =>
+      a.map(x => {
+        // hardcoding LGB to LAX
+        if (x === "LGB") return "LAX";
+        else return x;
+      })
+    )
+    .then(extracted => {
+      if (_.isEqual(extracted.sort(), provider.pops)) {
+        console.log(
+          `${asset}:success`,
+          extracted.length - provider.pops.length
+        );
+      } else {
+        toTelegram(asset);
+        console.log(`${asset}:fail`, extracted.length - provider.pops.length);
+        console.log(
+          `added`,
+          extracted.filter(e => !provider.pops.includes(e))
+        );
+        console.log(
+          `removed`,
+          provider.pops.filter(e => !extracted.includes(e))
+        );
+      }
+    });
 };
 
-spotter()
-  .then(value => value.match(/\((.{3,7})\)/gm).map(x => x.slice(1, 4)))
-  .then(a => _.uniq(a))
-  .then(a =>
-    a.map(x => {
-      // hardcoding LON to LGW
-      if (x === "LON") return "LGW";
-      else return x;
-    })
-  )
-  .then(a =>
-    a.map(x => {
-      // hardcoding WDC to LEE
-      if (x === "WDC") return "LEE";
-      else return x;
-    })
-  )
-  .then(a => a.concat(["LAX", "LGB"]))
-
-  .then(extracted => {
-    if (_.isEqual(extracted.sort(), provider.pops)) {
-      console.log(`${asset}:success`, extracted.length - provider.pops.length);
-    } else {
-      toTelegram(asset);
-      console.log(`${asset}:fail`, extracted.length - provider.pops.length);
-      console.log(
-        `added`,
-        extracted.filter(e => !provider.pops.includes(e))
-      );
-      console.log(
-        `removed`,
-        provider.pops.filter(e => !extracted.includes(e))
-      );
-    }
-  });
+spotter();
